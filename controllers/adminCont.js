7967639f2e845ab5b38eed9a125707b4bf3c6f83
saurav <sauravrijal30@gmail.com>
2021-06-28 18:31:11 +0545

    added create posts form to admin area

--- controllers/adminCont.js
+++ controllers/adminCont.js
@@ -1,5 +1,14 @@
 module.exports= {
     admin: (req,res)=>{
         res.render("admin");
+    },
+    getPosts:(req,res)=>{
+        res.send("All posts")
+    },
+    submitPosts:(req,res)=>{
+        res.send("post submitted")
+    },
+    createPosts:(req,res)=>{
+        res.render("createPostAdmin");
     }
 }
--- src/routes/adminroutes.js
+++ src/routes/adminroutes.js
@@ -5,8 +5,10 @@ const adminCont = require("../../controllers/adminCont");
 
 router.route("/")
     .get(adminCont.admin)
-
-
-
+router.route("/posts")
+    .get(adminCont.getPosts)
+    .post(adminCont.submitPosts)
+router.route("/posts/create")
+    .get(adminCont.createPosts);
 
 module.exports = router;
